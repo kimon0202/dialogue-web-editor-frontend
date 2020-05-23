ff3fe3a737e1e11f619342a0c7326b3326bc1124
Gustavo Bauke <baukegustavo@gmail.com>
2020-05-23 19:54:20 -0300

    Updates

---
+++ precache-manifest.e0b029c6048c4c81c6528d1650d79027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2829834df4528a4e0ca996018fc006a8",
+    "url": "/dialogue-web-editor-frontend/index.html"
+  },
+  {
+    "revision": "76f5d356d858aac8e044",
+    "url": "/dialogue-web-editor-frontend/static/css/main.29ce97ea.chunk.css"
+  },
+  {
+    "revision": "3662c4bc0aefad5b9d8c",
+    "url": "/dialogue-web-editor-frontend/static/js/2.1cb0afcd.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/dialogue-web-editor-frontend/static/js/2.1cb0afcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f5d356d858aac8e044",
+    "url": "/dialogue-web-editor-frontend/static/js/main.cae2a998.chunk.js"
+  },
+  {
+    "revision": "eaa8903fc781c679941d",
+    "url": "/dialogue-web-editor-frontend/static/js/runtime-main.3bf627d7.js"
+  }
+]);
